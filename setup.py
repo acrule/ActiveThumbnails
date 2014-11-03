bbe2cf7c0ac5a75d38f0847164de46e4d758d732
acrule <acrule@gmail.com>
2014-11-03 08:36:32 -0800

    Modify setup.py to include py2app

--- setup.py
+++ setup.py
@@ -1,28 +1,25 @@
-# import sys
+import sys
+import os
 from setuptools import setup
 from setuptools import find_packages
 import glob #, subprocess
-# import os
 
 
 PROJECT = "Active Thumbnails"
-ICON = "Minimal.icns"
 
 plist = {
-    "CFBundleIconFile" : ICON,
-    "CFBundleIdentifier" : "com.minimal-project.%s" % PROJECT,
-    }
-
+    'iconfile':'Resources/eye.icns',
+}
 
 setup(
-    name = "Active Thumbnails",
-    version = "0.1",
-    packages = find_packages(),
-    author = "Adam Rule",
-    description = "A simple pyobjc project to get you started",
-    app=["Application.py"],
-    data_files=["English.lproj"] +glob.glob("Resources/*.*"),
-    options=dict(py2app=dict(
-        plist=plist,
-    )),
-)
+      name = "Active Thumbnails",
+      version = "0.1",
+      setup_requires=["py2app"],
+      options={'py2app': plist},
+      packages = find_packages(),
+      author = "Adam Rule",
+      description = "A simple pyobjc project to get you started",
+      app=["Application.py"],
+      data_files=["English.lproj"] +glob.glob("Resources/*.*"),
+      )
+
